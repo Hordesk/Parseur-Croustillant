20829539b323b13aba4317a771084e81566943f3
Clement Vannier <clement.vannier@free.fr>
2014-02-25 09:52:07 +0000

    Remise en place des fichiers precedents

---
+++ src/main/java/parseurCroustillant/Parseur.java
@@ -0,0 +1,21 @@
+package parseurCroustillant;
+
+import org.tsaap.questions.Quiz;
+
+public class Parseur {
+
+	public String setInput(String input) {
+		return "";
+	}
+
+	public Quiz parse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Quiz getQuiz() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/parseurCroustillant/Run.java
@@ -0,0 +1,9 @@
+package parseurCroustillant;
+
+public class Run {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
